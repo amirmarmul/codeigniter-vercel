c7b98e52790dc237dc873a29f845123c0fb46675
amirmarmul <amiruddinmarmul@gmail.com>
2021-02-18 21:06:37 +0800

    redirect

--- application/controllers/Php.php
+++ application/controllers/Php.php
@@ -3,6 +3,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Php extends CI_Controller 
 {
+	public function index()
+	{
+		$this->info();
+	}
+
 	public function info()
 	{
 		return phpinfo();
